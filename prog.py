6e202762f1d1e00a52cc79fe833240d77543c486
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2021-11-22 21:26:57 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -2,15 +2,12 @@
 import math
 
 #code for checking if a number is perfect square in given range
-def check_perfect_square():
-    n = 35
-    if(n<1) return []
+def check_perfect_square(n):
+    if(n<1):
+        return []
     l = []
-    for i in range(1,35):
+    for i in range(1,n):
         root = int(math.sqrt(i))
         if(root*root == i):
             l.append(i)
     return l
-
-print(check_perfect_square())
-
