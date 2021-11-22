c427981975c7cae3f039c6c557780eeae33f20e6
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2021-11-22 21:40:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,18 @@ import unittest
 from prog import check_perfect_square
 
 class TestSquare(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test case to add two numbers
-        """
+    def test_list_int_0(self):
         data = 15
         result = check_perfect_square(data)
         self.assertEqual(result, [1,4,9])
+    def test_list_int_1(self):
+        data = 25
+        result = check_perfect_square(data)
+        self.assertEqual(result, [1,4,9,16,25])
+    def test_list_int_2(self):
+        data = -10
+        result = check_perfect_square(data)
+        self.assertEqual(result, [])
 
 if __name__ == '__main__':
     unittest.main()
